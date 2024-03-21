69b35b84d4f6af153ce9daceb48f8d2e41adf985
Vedang-jombay <vedang.avaghade@jombay.com>
2024-03-21 12:56:37 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,227 @@
+import 'dart:convert';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+void main() {
+  runApp(MyApp());
+}
+
+class Character {
+  final int id;
+  final String name;
+  final String status;
+  final String species;
+  final String gender;
+  final String image;
+  final String lastKnownLocation;
+  final String firstKnownLocation;
+
+  Character({
+    required this.id,
+    required this.name,
+    required this.status,
+    required this.species,
+    required this.gender,
+    required this.image,
+    required this.lastKnownLocation,
+    required this.firstKnownLocation,
+  });
+
+  factory Character.fromJson(Map<String, dynamic> json) {
+    return Character(
+      id: json['id'],
+      name: json['name'],
+      status: json['status'],
+      species: json['species'],
+      gender: json['gender'],
+      image: json['image'],
+      lastKnownLocation: json['location']['name'],
+      firstKnownLocation: json['origin']['name'],
+    );
+  }
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Rick and Morty Characters',
+      theme: ThemeData(
+        scaffoldBackgroundColor: Colors.black87,
+        primarySwatch: Colors.blue,
+      ),
+      home: MainScreen(),
+    );
+  }
+}
+
+class MainScreen extends StatefulWidget {
+  @override
+  _MainScreenState createState() => _MainScreenState();
+}
+
+class _MainScreenState extends State<MainScreen> {
+  late Future<List<Character>> futureCharacters;
+
+  @override
+  void initState() {
+    super.initState();
+    futureCharacters = fetchCharacters();
+  }
+
+  Future<List<Character>> fetchCharacters() async {
+    final response = await http.get(Uri.parse('https://rickandmortyapi.com/api/character'));
+    if (response.statusCode == 200) {
+      final List<dynamic> jsonData = jsonDecode(response.body)['results'];
+      return jsonData.map((character) => Character.fromJson(character)).toList();
+    } else {
+      throw Exception('Failed to load characters');
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Rick and Morty Characters'),
+      ),
+      body: FutureBuilder<List<Character>>(
+        future: futureCharacters,
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            List<Character> characters = snapshot.data!;
+            return ListView.builder(
+              itemCount: characters.length,
+              itemBuilder: (context, index) {
+                return GestureDetector(
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => CharacterDetailsScreen(character: characters[index]),
+                      ),
+                    );
+                  },
+                  child: SizedBox(
+                    height: 150,
+                    child: Card(
+                      color: Colors.grey[900],
+                      child: Row(
+                        children: [
+                          AspectRatio(
+                            aspectRatio: 1,
+                            child: Image.network(
+                              characters[index].image,
+                              fit: BoxFit.cover,
+                            ),
+                          ),
+                          SizedBox(width: 16),
+                          Expanded(
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                Text(
+                                  characters[index].name,
+                                  style: TextStyle(color: Colors.white),
+                                ),
+                                SizedBox(height: 8),
+                                Row(
+                                  children: [
+                                    Container(
+                                      width: 10,
+                                      height: 10,
+                                      decoration: BoxDecoration(
+                                        color: characters[index].status == 'Alive' ? Colors.green : Colors.red,
+                                        shape: BoxShape.circle,
+                                      ),
+                                    ),
+                                    SizedBox(width: 5),
+                                    Text(
+                                      '${characters[index].status} - ${characters[index].species}',
+                                      style: TextStyle(color: Colors.white),
+                                    ),
+                                  ],
+                                ),
+                                SizedBox(height: 4),
+                                Text(
+                                  'Last known location: ${characters[index].lastKnownLocation}',
+                                  style: TextStyle(color: Colors.white),
+                                ),
+                                SizedBox(height: 4),
+                                Text(
+                                  'First seen in: ${characters[index].firstKnownLocation}',
+                                  style: TextStyle(color: Colors.white),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                );
+              },
+            );
+          } else if (snapshot.hasError) {
+            return Center(
+              child: Text("${snapshot.error}"),
+            );
+          }
+          return Center(
+            child: CircularProgressIndicator(),
+          );
+        },
+      ),
+    );
+  }
+}
+
+class CharacterDetailsScreen extends StatelessWidget {
+  final Character character;
+
+  CharacterDetailsScreen({required this.character});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(character.name),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Image.network(character.image),
+            SizedBox(height: 16),
+            Text(
+              'Name: ${character.name}',
+              style: TextStyle(color: Colors.white),
+            ),
+            Text(
+              'Status: ${character.status}',
+              style: TextStyle(color: Colors.white),
+            ),
+            Text(
+              'Species: ${character.species}',
+              style: TextStyle(color: Colors.white),
+            ),
+            Text(
+              'Gender: ${character.gender}',
+              style: TextStyle(color: Colors.white),
+            ),
+            Text(
+              'Last Known Location: ${character.lastKnownLocation}',
+              style: TextStyle(color: Colors.white),
+            ),
+            Text(
+              'First Seen in: ${character.firstKnownLocation}',
+              style: TextStyle(color: Colors.white),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
